a7ba879e3226d407628a11760134521f46133a4f
kevin_xie <xie.kevin.j@gmail.com>
2020-08-05 14:03:06 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,4 @@
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: "Kevin", last_name: "Xie")
